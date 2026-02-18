36515f92f51bd1c7257a9cbeb5680ec1efe3afa9
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 11:03:11 +0100

    Update listaTareas.java

--- src/listaTareas.java
+++ src/listaTareas.java
@@ -1,13 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class listaTareas {
+
+    public static void main(String[] args) {
+        List <String> listaTareas = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+        do {
+            System.out.println("Elige una de las siguientes opciones; ");
+            System.out.println("Opción 1: Añadir una tarea");
+            System.out.println("Opción 2: Añadir la tarea en una posicion concreta");
+            System.out.println("Opción 3: Eliminar una tarea segun la posicion de la misma");
+            System.out.println("Opción 4: Mostrar todas las tareas");
+            System.out.println("Opción 0: Salir");
+            opcion= sc.nextInt();
+            opcion= Integer.parseInt(sc.nextLine());
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Introduce el nombre de la tarea");
+                    String tareanueva = sc.nextLine();
+                    listaTareas.add(tareanueva);
+                    break;
+
+                case 2:
+                    System.out.println("Introduce el nombre de la tarea ");
+                    String tareaposicionada = sc.nextLine();
+                    System.out.println("Introduce la posición donde se encontrará");
+                    int posiciontarea = sc.nextInt();
+
+                    if (posiciontarea >= 0 && posiciontarea<=listaTareas.size()){
+
+                    }else {
+                        System.out.println("Fuera de rango");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Posición a eliminar: ");
+                    int posElim = Integer.parseInt(sc.nextLine());
+
+                    if (posElim >= 0 && posElim < listaTareas.size()) {
+                        listaTareas.remove(posElim);
+                        System.out.println("Tarea eliminada.");
+                    } else {
+                        System.out.println("Error: Posición no válida.");
+                    }
+                    break;
+
+                case 4:
+                    if (listaTareas.isEmpty()) {
+                        System.out.println("La lista está vacía.");
+                    } else {
+                        for (int i = 0; i < listaTareas.size(); i++) {
+                            System.out.println(i + ". " + listaTareas.get(i));
+                        }
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+
+            }
+        }while (opcion != 0);
+    }
 }
